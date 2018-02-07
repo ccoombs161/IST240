4a882132679111411b175167c88412de55a750f6
ccoombs161 <cameron.coombs97@gmail.com>
2018-02-06 19:53:17 -0500

    Created BootstrapIntroduction
    Code for labs 4.1,4.2 using MVC Controller and Views

---
+++ BootstrapIntroduction/BootstrapIntroduction/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BootstrapIntroduction.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult CalcApp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CalcApp(string ValA, String ValB, String Op)
+        {
+            int a = Convert.ToInt32(ValA);
+            int b = Convert.ToInt32(ValB);
+            int ans = 0;
+
+            switch(Op)
+            {
+                case "Add":
+                    ans = a + b;
+                    break;
+                case "Sub":
+                    ans = a - b;
+                    break;
+                case "Multi":
+                    ans = a * b;
+                    break;
+                case "Div":
+                    ans = a / b;
+                    break;
+            }
+
+            string model = Convert.ToString(ans);
+
+            return View("CalcApp", (object)model);
+        }
+    }
+}
